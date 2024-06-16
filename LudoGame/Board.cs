c93cf448bdabd24c4dfaa0ac33de1204bb15d107
Maie- <49562104+Maie-r@users.noreply.github.com>
2024-06-15 21:15:34 -0300

    Update Board.cs

--- LudoGame/Board.cs
+++ LudoGame/Board.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,19 +17,18 @@ namespace LudoGame
                 {
                     pawns[j, i] = new Pawn(j, i);
                 }
-                Console.WriteLine();
             }
-            if (debug == "debug")
+            if (debug == "debug") //
             {
                 // set up for capture
-                pawns[0, 0].Move(12);
-                pawns[1, 0].Move(11);
-                pawns[2, 0].Move(10);
-                pawns[3, 0].Move(9);
-                pawns[0, 1].Move(1);
+                pawns[0, 0].Move(27);
+                pawns[1, 0].Move(26);
+                pawns[2, 0].Move(25);
+                pawns[3, 0].Move(24);
+                pawns[0, 1].Move(2);
                 pawns[1, 1].Move(2);
-                pawns[2, 1].Move(3);
-                pawns[3, 1].Move(4);
+                pawns[2, 1].Move(2);
+                pawns[3, 1].Move(2);
             }
         }
 
@@ -38,54 +37,49 @@ namespace LudoGame
             pawn.Move(amount);
         }
 
-        public void BoardStatus()
+        public void BoardStatus(LogWriter log)
         {
             for (int i = 0; i < pawns.GetLength(0); i++)
             {
                 for (int j = 0; j < pawns.GetLength(1); j++)
                 {
-                    //Console.Write(GameState[i, j] + " ");
-                    Console.Write(pawns[i, j].pos + " ");
+                    log.Write(pawns[i, j].pos + " ");
                 }
-                Console.WriteLine();
+                log.WriteLine("");
             }
         }
 
-        public bool PieceEaten(int lastmoved, int player)
+        public bool PieceEaten(int lastmoved, int player, LogWriter log)
         {
-            BoardStatus();
+            BoardStatus(log);
             int pos = pawns[lastmoved - 1, player].pos;
             switch (player)
             {
                 case 0:
-                    int[] offsets1 = {0, 13, 26, 39};
-                    if (CheckOverlap(player, pos, offsets1))
+                    int[] offsets1 = {0, 26, 13, 39};
+                    if (CheckOverlap(player, pos, offsets1, log))
                     {
-                        Console.WriteLine("Peça comida!");
                         return true;
                     }
                     break;
                 case 1:
-                    int[] offsets2 = { 39, 0, 13, 26  };
-                    if (CheckOverlap(player, pos, offsets2))
+                    int[] offsets2 = { 26, 0, 39, 13};
+                    if (CheckOverlap(player, pos, offsets2, log))
                     {
-                        Console.WriteLine("Peça comida!");
                         return true;
                     }
                     break;
                 case 2:
-                    int[] offsets3 = { 26, 36, 0, 13 };
-                    if (CheckOverlap(player, pos, offsets3))
+                    int[] offsets3 = { 39, 13, 0, 26 };
+                    if (CheckOverlap(player, pos, offsets3, log))
                     {
-                        Console.WriteLine("Peça comida!");
                         return true;
                     }
                     break;
                 case 3:
-                    int[] offsets4 = { 13, 26, 36, 0 };
-                    if (CheckOverlap(player, pos, offsets4))
+                    int[] offsets4 = { 13, 39, 26, 0 };
+                    if (CheckOverlap(player, pos, offsets4, log))
                     {
-                        Console.WriteLine("Peça comida!");
                         return true;
                     }
                     break;
@@ -93,9 +87,10 @@ namespace LudoGame
             return false;
         }
 
-        bool CheckOverlap(int player, int pos, int[] offsets)
+        bool CheckOverlap(int player, int pos, int[] offsets, LogWriter log)
         {
-            int failsafe = 0; // TO DO: CHECK FOR PROTECTED AREAS
+            int failsafe = 0;
+            bool result = false;
             if (player == 0)
             {
                 failsafe++;
@@ -104,10 +99,18 @@ namespace LudoGame
             {
                 for (int j = 0; j < pawns.GetLength(0); j++)
                 {
-                    if (pos == (pawns[j, i].pos + offsets[i]))
+                    if (pos <= (1 + offsets[i]) || pos == (9 + offsets[i]) || pos >= 53)
                     {
-                        pawns[j, i].Return();
-                        return true;
+                        return false;
+                    }
+                    else
+                    {
+                        if (pos == (pawns[j, i].pos + offsets[i]))
+                        {
+                            log.WriteLine($"Comeu o peão {pawns[j, i].id + 1} do jogador {pawns[j, i].team + 1}!");
+                            pawns[j, i].Return();
+                            result = true;
+                        }
                     }
                 }
                 if ((i + 1) == player)
@@ -115,7 +118,7 @@ namespace LudoGame
                     i++;
                 }
             }
-            return false;
+            return result;
         }
     }
 }
