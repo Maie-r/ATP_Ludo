a1bb55f0d595ac5a5b6c9e5bf99d2be71e9726dc
Maie- <49562104+Maie-r@users.noreply.github.com>
2024-06-18 20:17:37 -0300

    fixed capture detection

--- LudoGame/Board.cs
+++ LudoGame/Board.cs
@@ -21,14 +21,14 @@ namespace LudoGame
             if (debug == "debug") //
             {
                 // set up for capture
-                pawns[0, 0].Move(27);
-                pawns[1, 0].Move(26);
-                pawns[2, 0].Move(25);
-                pawns[3, 0].Move(24);
-                pawns[0, 1].Move(2);
-                pawns[1, 1].Move(2);
-                pawns[2, 1].Move(2);
-                pawns[3, 1].Move(2);
+                pawns[0, 0].Move(9);
+                pawns[1, 0].Move(8);
+                pawns[2, 0].Move(7);
+                pawns[3, 0].Move(6);
+                pawns[0, 1].Move(36);
+                pawns[1, 1].Move(36);
+                pawns[2, 1].Move(35);
+                pawns[3, 1].Move(37);
             }
         }
 
@@ -56,28 +56,28 @@ namespace LudoGame
             switch (player)
             {
                 case 0:
-                    int[] offsets1 = {0, 26, 13, 39};
+                    int[] offsets1 = {0, 26, 13, 39, 0, -26, 39, 13};
                     if (CheckOverlap(player, pos, offsets1, log))
                     {
                         return true;
                     }
                     break;
                 case 1:
-                    int[] offsets2 = { 26, 0, 39, 13};
+                    int[] offsets2 = { 26, 0, 39, 13, -26, 0, 13, 39};
                     if (CheckOverlap(player, pos, offsets2, log))
                     {
                         return true;
                     }
                     break;
                 case 2:
-                    int[] offsets3 = { 39, 13, 0, 26 };
+                    int[] offsets3 = { 39, 13, 0, 26, 13, 39, 0, -26};
                     if (CheckOverlap(player, pos, offsets3, log))
                     {
                         return true;
                     }
                     break;
                 case 3:
-                    int[] offsets4 = { 13, 39, 26, 0 };
+                    int[] offsets4 = { 13, 39, 26, 0, 39, 13, -26, 0};
                     if (CheckOverlap(player, pos, offsets4, log))
                     {
                         return true;
@@ -99,18 +99,32 @@ namespace LudoGame
             {
                 for (int j = 0; j < pawns.GetLength(0); j++)
                 {
-                    if (pos <= (1 + offsets[i]) || pos == (9 + offsets[i]) || pos >= 53)
+                    if (pos <= 1 || pos == 9 || pos == 14 || pos == 22 || pos == 27 || pos == 35 || pos == 40 || pos == 48 || pos >= 53)
                     {
+                        Console.WriteLine("Protegido!");
                         return false;
                     }
                     else
                     {
-                        if (pos == (pawns[j, i].pos + offsets[i]))
+                        if (pos > pawns[j, i].pos)
                         {
-                            log.WriteLine($"Comeu o peão {pawns[j, i].id + 1} do jogador {pawns[j, i].team + 1}!");
-                            pawns[j, i].Return();
-                            result = true;
+                            if (pos == (pawns[j, i].pos + offsets[i]))
+                            {
+                                log.WriteLine($"Comeu o peão {pawns[j, i].id + 1} do jogador {pawns[j, i].team + 1}!");
+                                pawns[j, i].Return();
+                                result = true;
+                            }
                         }
+                        else
+                        {
+                            if (pos == (pawns[j, i].pos + offsets[i+4]))
+                            {
+                                log.WriteLine($"Comeu o peão {pawns[j, i].id + 1} do jogador {pawns[j, i].team + 1}!");
+                                pawns[j, i].Return();
+                                result = true;
+                            }
+                        }
+                        
                     }
                 }
                 if ((i + 1) == player)
